b6c06b5c12672afa5985056c9529fd71fbd22c0c
sushma82 <sushmareddy8290@gmail.com>
2021-10-01 10:33:18 +0530

    add reusable carousel

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import DefaultHOC from "./HOC/Default.HOC";
 
 // Pages
 import HomePage from "./pages/Home.page";
+import Movie from "./pages/Movie.page";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <DefaultHOC path="/" exact component={HomePage} />
+      <DefaultHOC path="/" exact component={Movie} />
     </>
   );
 }
