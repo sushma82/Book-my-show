07daf12c06fb29535f5729afe60afd0cdbfc77c5
sushma82 <sushmareddy8290@gmail.com>
2021-10-01 10:50:05 +0530

    add movie layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route } from "react-router-dom";
+
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
 
 // Pages
 import HomePage from "./pages/Home.page";
 import Movie from "./pages/Movie.page";
-
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,7 +14,7 @@ function App() {
   return (
     <>
       <DefaultHOC path="/" exact component={HomePage} />
-      <DefaultHOC path="/" exact component={Movie} />
+      <MovieHOC path="/movie/:id" exact component={Movie} />
     </>
   );
 }
