d92630f7c262ffc5ee9e006f95a01e1d4d514552
sushma82 <sushmareddy8290@gmail.com>
2021-10-02 17:27:46 +0530

    add movie context

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 // Components
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
 import PosterSlider from "../components/PosterSlider/PosterSlider.component";
-
+// config
+import PosterSlider from "../components/PosterSlider/PosterSlider.component";
 // config
 import TempPosters from "../config/TempPosters.config";
 
 const HomePage = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+
+  useEffect(() => {
+    const requestPopularMovies = async () => {
+      const getPopularMovies = await axios.get("/movie/popular");
+      setPopularMovies(getPopularMovies.data.results);
+    };
+
+    requestPopularMovies();
+  }, []);
+
+  useEffect(() => {
+    const requestTopRatedMovies = async () => {
+      const getTopRatedMovies = await axios.get("/movie/top_rated");
+      setTopRatedMovies(getTopRatedMovies.data.results);
+    };
+
+    requestTopRatedMovies();
+  }, []);
+
+  useEffect(() => {
+    const requestUpcomingMovies = async () => {
+      const getUpcomingMovies = await axios.get("/movie/upcoming");
+      setUpcomingMovies(getUpcomingMovies.data.results);
+    };
+
+    requestUpcomingMovies();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col gap-10">
@@ -17,7 +50,20 @@ const HomePage = () => {
           </h1>
           <EntertainmentCardSlider />
         </div>
-
+        
+        <div className="bg-bms-800 py-12 ">
+          <div className="container mx-auto px-4 flex flex-col gap-3">
+            <div className="hidden md:flex">
+              <img
+                src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+                alt="Rupay"
+                className="w-full h-full"
+        <div className="container mx-auto px-4 ">
+          <h1 className="text-2xl font-bold text-gray-800 my-3">
+            The best of Entertainment
+          </h1>
+          <EntertainmentCardSlider />
+        </div>
         <div className="bg-bms-800 py-12 ">
           <div className="container mx-auto px-4 flex flex-col gap-3">
             <div className="hidden md:flex">
@@ -28,7 +74,7 @@ const HomePage = () => {
               />
             </div>
             <PosterSlider
-              images={TempPosters}
+              images={popularMovies}
               title="Premieres"
               subtitle="Brand new relases every friday"
               isDark
@@ -36,20 +82,29 @@ const HomePage = () => {
           </div>
         </div>
       </div>
+        
+          </div>
+        </div>
+      </div>
 
       <div className="container mx-auto px-4 my-8">
         <PosterSlider
-          images={TempPosters}
+          images={topRatedMovies}
           title="Online Streaming events"
           isDark={false}
         />
       </div>
       <div className="container mx-auto px-4 my-8">
         <PosterSlider
-          images={TempPosters}
+          images={upcomingMovies}
           title="Outdoor events"
           isDark={false}
         />
+      </div>
+    </>
+  );
+};
+export default HomePage;
       </div>
     </>
   );
